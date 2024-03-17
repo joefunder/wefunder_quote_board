b16a3d289802d30240409a53dc7f0b070386e1be
Joe Berusch <joeberusch@gmail.com>
2024-03-17 16:52:22 -0700

    removed slack api token

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -3,7 +3,7 @@
 require 'slack-ruby-client'
 
 Slack.configure do |config|
-  config.token = 'xoxb-4806305733-6827226971729-IYvchweCXXvkIB1869r6q71s'
+  config.token = ENV['SLACK_API_TOKEN']
 end
 
 # Optionally, you can also specify other configuration options here if needed
